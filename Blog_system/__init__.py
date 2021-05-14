cf2ae1e45a771e35cdcf1a98e2157f4b527d4622
root <751359244@qq.com>
2021-05-14 21:12:23 +0800

    firstvs

---
+++ Blog_system/__init__.py
@@ -0,0 +1,7 @@
+# 进入虚拟环境
+# env\Scripts\activate.bat
+# 运行服务器
+# python manage.py runserver
+# 数据迁移
+# 1.python manage.py makemigrations
+# 2.python manage.py migrate
---
+++ article/urls.py
@@ -0,0 +1,30 @@
+# 引入path
+from django.urls import path
+from . import views
+
+# 正在部署的应用的名称
+app_name = 'article'
+
+urlpatterns = [
+    # 文章列表
+    path('article-list/', views.article_list, name='article_list'),
+    # 文章详情
+    path('article-detail/<int:id>/', views.article_detail, name='article_detail'),
+    # 写文章
+    path('article-publish/', views.article_publish, name='article_publish'),
+    # 删除文章
+    path('article-delete/<int:id>/', views.article_delete, name='article_delete'),
+    # 更新文章
+    path('article-update/<int:id>/', views.article_update, name='article_update'),
+    # 点赞 +1
+    path('increase-likes/<int:id>/', views.IncreaseLikesView.as_view(), name='increase_likes'),
+    # # 点赞 +1
+    # path('increase-likes/<int:id>/', views.IncreaseLikesView.as_view(), name='increase_likes'),
+    #
+    # # 列表类视图
+    # path('list-view/', views.ArticleListView.as_view(), name='list_view'),
+    # # 详情类视图
+    # path('detail-view/<int:pk>/', views.ArticleDetailView.as_view(), name='detail_view'),
+    # # 创建类视图
+    # path('create-view/', views.ArticleCreateView.as_view(), name='create_view'),
+]
